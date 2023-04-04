3434c7a7880ac1543ec52f3caa7d05974568a5db
fantasiaproducciones <129820812+fantasiaproducciones@users.noreply.github.com>
2023-04-03 23:48:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -192,7 +192,7 @@ function actualizarTotalCarrito(){
     }
     total = Math.round(total * 100)/100;
 
-    document.getElementsByClassName('carrito-precio-total')[0].innerText = 'S/.'+total.toLocaleString("es") + ",00";
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = 'S/.'+total.toLocaleString("es");
 
 }
 
